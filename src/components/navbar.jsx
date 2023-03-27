41612b8f2298e722ebb2520a2024b7449d535718
Yifei Xiao <yifeixiaoo@gmail.com>
2023-03-27 17:29:08 -0600

    add shop and cart pages

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { ShoppingCart } from "phosphor-react";
+import "./navbar.css"
 
 export const Navbar = () => {
-    return <div>NavBar</div>
+    return <div className="navbar">
+        <div className="links">
+            <Link to="/"> Shop </Link>
+            <Link to="/cart">
+                <ShoppingCart size={32} />
+            </Link>
+        </div>
+    </div>
 };
