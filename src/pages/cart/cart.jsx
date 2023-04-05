fbff3598d9c480b743148dcccb45e0760bf2590d
Yifei Xiao <yifeixiaoo@gmail.com>
2023-04-04 18:20:01 -0600

    styling for cart

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { PRODUCTS } from "../../products";
 import { ShopContext } from "../../context/shop-context";
 import { CartItem } from "./cartItem";
+import "./cart.css"
 
 export const Cart = () => {
     const { cartItems } = useContext(ShopContext);
