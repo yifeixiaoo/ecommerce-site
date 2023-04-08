f5fa4de8060209f89d9dddeb592197a73374c65e
Yifei Xiao <yifeixiaoo@gmail.com>
2023-04-07 22:45:18 -0600

    update navbar styling

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,14 +2,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { ShoppingCart } from "phosphor-react";
 import "./navbar.css"
+import minishopLogo from "../images/logos/minishop-yx-default-small.png"
 
 export const Navbar = () => {
-    return <div className="navbar">
-        <div className="links">
-            <Link to="/shop"> Shop </Link>
-            <Link to="/cart">
-                <ShoppingCart size={32} />
-            </Link>
-        </div>
-    </div>
+    // return <div className="navbar">
+    //     <div className="links">
+    //         <Link to="/" className="home"> Home </Link>
+    //         <Link to="/shop"> Shop </Link>
+    //         <Link to="/cart">
+    //             <ShoppingCart size={32} />
+    //         </Link>
+    //     </div>
+    // </div>
+    return <header>
+        <Link to="/"> <img src={minishopLogo} alt="logo" className="logo-img"/> </Link>
+        <nav>
+            <ul className="nav__links">
+                <li><Link to="/shop"> Shop </Link></li>
+                <li><Link to="/cart"><ShoppingCart size={20} /></Link></li>
+            </ul>
+        </nav>
+    </header>
 };
