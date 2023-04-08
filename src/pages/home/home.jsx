668b8f62ed4c1aa7441c7dc300e2100c1ccaa736
Yifei Xiao <yifeixiaoo@gmail.com>
2023-04-07 23:52:31 -0600

    update home page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import product1 from "../../images/products/1.png"
+import "./home.css"
 
 export const Home = () => {
-    return <div className="home">
-        Home
-    </div>
+    return (
+        <>
+          <div className="home-container">
+            <div className="container">
+              <div className="grid-container">
+                <div className="featured grid-one">
+                  <Link to="/">
+                    <div id="img1" className="lil-overlay"></div>
+                    <img src={product1} alt="img1" />
+                    <p className="main-description">Lighting</p>
+                  </Link>
+                </div>
+                <div className="featured grid-two">
+                  <Link to="/">
+                    <div id="img2" className="lil-overlay"></div>
+                    <img src={product1} alt="img2" />
+                    <p className="main-description">Kitchen</p>
+                  </Link>
+                </div>
+                <div className="featured grid-four">
+                  <Link to="/">
+                    <div id="img3" className="lil-overlay"></div>
+                    <img src={product1} alt="img3" />
+                    <p className="main-description">Living</p>
+                  </Link>
+                </div>
+                <div className="featured grid-four-low">
+                  <Link to="/">
+                    <div id="img4" className="lil-overlay"></div>
+                    <img src={product1} alt="img4" />
+                    <p className="main-description">Bath</p>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      );
 };
