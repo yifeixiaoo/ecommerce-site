7cbf118b0239f11d7ecb8c149ae7102a4468210a
Yifei Xiao <yifeixiaoo@gmail.com>
2023-04-09 16:17:02 -0600

    add product page template

--- src/pages/productPage/productPage.jsx
+++ src/pages/productPage/productPage.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import "./productPage.css"
+import img1 from "../../images/products/1.png"
+
 
 export const ProductPage = () => {
     return <div className="productPage">
-        Product
+        <div className="productImg">
+            <img src={img1}/>
+        </div>
+        <div className="productInfo">
+            <h2> Product1 </h2>
+            <p> $999.99 </p>
+        </div>
     </div>
 };
