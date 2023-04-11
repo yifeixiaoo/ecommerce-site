8d07e326bcda1e778f727444851dcd1f5c91f2bc
Yifei Xiao <yifeixiaoo@gmail.com>
2023-04-10 18:20:30 -0600

    update product page styling, replace logo png with transparent background, update navbar styling

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,18 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { ShoppingCart } from "phosphor-react";
 import "./navbar.css"
-import minishopLogo from "../images/logos/minishop-yx-default-small.png"
+import minishopLogo from "../images/logos/minishop-yx-default-transparent.png.png"
 
 export const Navbar = () => {
-    // return <div className="navbar">
-    //     <div className="links">
-    //         <Link to="/" className="home"> Home </Link>
-    //         <Link to="/shop"> Shop </Link>
-    //         <Link to="/cart">
-    //             <ShoppingCart size={32} />
-    //         </Link>
-    //     </div>
-    // </div>
     return <header>
         <Link to="/"> <img src={minishopLogo} alt="logo" className="logo-img"/> </Link>
         <nav>
--- src/pages/productPage/productPage.jsx
+++ src/pages/productPage/productPage.jsx
@@ -4,13 +4,54 @@ import img1 from "../../images/products/1.png"
 
 
 export const ProductPage = () => {
-    return <div className="productPage">
-        <div className="productImg">
-            <img src={img1}/>
-        </div>
-        <div className="productInfo">
-            <h2> Product1 </h2>
-            <p> $999.99 </p>
-        </div>
-    </div>
+    return (
+        <>
+            <div className="product-page-div">
+                <div className="container">
+                    <div className="product-div">
+                        <div className="product-left">
+                            <div className="big-img">
+                                <img src={img1} alt="product" />
+                            </div>
+                        </div>
+                        <div className="product-right">
+                            <h3 className="product-big-name">product spec</h3>
+                            <div className="product-quant">
+                                <p>Quantity</p>
+                                <div className="product-btns">
+                                    <button>-</button>
+                                    <p className="quantity"> 1 </p>
+                                    <button>+</button>
+                                </div>
+                                <p className="product-price"> price </p>
+                            </div>
+                            <div className="atc-buy">
+                                <button
+                                    className="atc-btn"
+                                >
+                                    add to cart
+                                </button>
+                                <button className="buy-btn">buy now</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="specifications">
+                        <div className="spec">
+                            <p className="spec-title">Texture:</p>
+                            <p className="title-desc">texture</p>
+                        </div>
+                        <div className="spec">
+                            <p className="spec-title">Weight:</p>
+                            <p className="title-desc">weight</p>
+                        </div>
+                        <div className="spec">
+                            <p className="spec-title">Size:</p>
+                            <p className="title-desc">size</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
