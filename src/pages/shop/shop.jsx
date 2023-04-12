4090665b85ddc6a69674de8357303028bba3a3b3
Yifei Xiao <yifeixiaoo@gmail.com>
2023-04-11 22:54:32 -0600

    add shopping page cover photo

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -4,14 +4,18 @@ import { Product } from "./product";
 import { Link } from "react-router-dom";
 import { CategoriesHeader } from "./categories/categories-header";
 import "./shop.css"
+import shopCover from "../../images/banner/shop-banner.avif"
 
 export const Shop = () => {
     return <div className="shop">
-        <div className="shopTitle">
-            <h1> Minimal Living </h1>
-        </div>
-        <CategoriesHeader />
         <div className="container">
+            <div className="shopCover">
+                <img src={shopCover} />
+                <div className="shopTitle">
+                    <h1> Minimal Living </h1>
+                </div>
+            </div>
+            <CategoriesHeader />
             <div className="products">
                 {PRODUCTS.map((product) => (
                     <Product data={product} />
